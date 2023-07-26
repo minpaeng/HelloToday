9dcba6d36529109096d476bd3458c2f3799b22a9
권민정 <22alswjd@naver.com>
2023-07-26 16:14:39 +0900

    refactor: BaseResponseDto 형식 변경

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/response/BaseResponseDto.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/response/BaseResponseDto.java
@@ -1,16 +1,17 @@
 package com.ssafy.hellotoday.api.response;
 
-import com.ssafy.hellotoday.common.util.constant.HttpStatusEnum;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class BaseResponseDto {
-    private HttpStatusEnum status;
+    private boolean success;
     private String message;
     private Object data;
 
     @Builder
-    public BaseResponseDto(HttpStatusEnum status, String message, Object data) {
-        this.status = status;
+    public BaseResponseDto(boolean success, String message, Object data) {
+        this.success = success;
         this.message = message;
         this.data = data;
     }
