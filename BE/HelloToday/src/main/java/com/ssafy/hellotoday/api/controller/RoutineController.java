6a70ee6e24d86d6fc808b20f690f3eb99dacb506
KodaHye <kodhdh09@gmail.com>
2023-07-26 13:42:01 +0900

    chore: swagger관련 주석 추가

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/controller/RoutineController.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/controller/RoutineController.java
@@ -4,6 +4,8 @@ import com.ssafy.hellotoday.api.response.routine.RoutineDetailResponseDto;
 import com.ssafy.hellotoday.api.response.routine.RoutineRecMentResponseDto;
 import com.ssafy.hellotoday.api.service.RoutineService;
 import com.ssafy.hellotoday.db.entity.routine.RecommendMent;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
+@Tag(name = "Routine", description = "루틴 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/routine")
@@ -20,10 +23,7 @@ public class RoutineController {
 
     private final RoutineService routineService;
 
-    /**
-     * 루틴 대분류 별 세분류 루틴 조회
-     * /1: 기본, /2: 정적, /3: 동적
-     */
+    @Operation(summary = "세분류 루틴 조회", description = "대분류 카테고리 별 세분류 루틴 조회 API<br>{categoryId} 1: 기본, 2: 정적, 3: 동적")
     @GetMapping("/detail/{categoryId}")
     public ResponseEntity<List<RoutineDetailResponseDto>> detailRoutine(@PathVariable Integer categoryId) {
         return new ResponseEntity<>(routineService.detailRoutine(categoryId), HttpStatus.OK);
@@ -33,6 +33,7 @@ public class RoutineController {
      * 루틴 대분류 별 추천 멘트 출력
      * 1: 기본, 2: 정적, 3: 동적
      */
+    @Operation(summary = "대분류 루틴 별 추천 멘트 조회", description = "대분류 카테고리 별 루틴 추천 멘트 조회 API<br>{categoryId} 1: 기본, 2: 정적, 3: 동적")
     @GetMapping("/ment/{categoryId}")
     public ResponseEntity<RoutineRecMentResponseDto> getRecommendMent(@PathVariable Integer categoryId) {
         return new ResponseEntity<>(routineService.getRecommendMent(categoryId), HttpStatus.OK);
