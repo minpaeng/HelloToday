b1a64120299f97d894726b4ba5b2cd40c92fe999
KodaHye <kodhdh09@gmail.com>
2023-07-30 22:39:05 +0900

    feat: QueryDSL 설정 적용

---
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/dto/routine/request/RoutineCheckRequestDto.java
@@ -0,0 +1,9 @@
+package com.ssafy.hellotoday.api.dto.routine.request;
+
+import com.ssafy.hellotoday.api.dto.routine.RoutineCheckDto;
+
+public class RoutineCheckRequestDto {
+    private Integer memberId;
+    private Integer routineDetailCatId;
+    private RoutineCheckDto routineCheckDto;
+}
---
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/db/repository/routine/RoutineCheckRepository.java
@@ -0,0 +1,6 @@
+package com.ssafy.hellotoday.db.repository.routine;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//public interface RoutineCheckRepository extends JpaRepository<RoutineCheck, Integer> {
+//}
