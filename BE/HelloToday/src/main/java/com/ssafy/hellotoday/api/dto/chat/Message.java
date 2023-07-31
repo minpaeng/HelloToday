8dbb317176c14ba1b5fb9b394c9e0673f64e210a
권민정 <22alswjd@naver.com>
2023-07-31 10:56:03 +0900

    feat: 채팅 컨트롤러 추가
    - 채팅 방 입장 메서드
    - 메세지 수신 및 발신 메서드

---
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/dto/chat/Message.java
@@ -0,0 +1,26 @@
+package com.ssafy.hellotoday.api.dto.chat;
+
+import com.ssafy.hellotoday.common.util.constant.MessageType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private MessageType type;
+    private int roomId;
+    private int senderId;
+    private String content;
+
+    public void newConnect() {
+        this.type = MessageType.ENTER;
+    }
+
+    public void newMessage() {
+        this.type = MessageType.TALK;
+    }
+}
