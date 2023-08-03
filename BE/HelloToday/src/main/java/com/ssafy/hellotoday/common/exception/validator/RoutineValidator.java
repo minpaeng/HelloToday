0d8685a7a3e55f052b382210aad395f4d28a6ba8
KodaHye <kodhdh09@gmail.com>
2023-08-03 17:19:29 +0900

    fix: 루틴 생성 예외 처리 수정

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/common/exception/validator/RoutineValidator.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/common/exception/validator/RoutineValidator.java
@@ -10,12 +10,16 @@ import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Component
 public class RoutineValidator {
     public void checkPrivateRoutineExist(RoutineRepository routineRepository, Member member) {
-        Routine checkRoutine = routineRepository.findByMember_MemberIdAndActiveFlag(member.getMemberId(), (byte) 1).get();
-        if(checkRoutine != null) throw CustomException.builder()
+
+        Optional<Routine> routine = routineRepository.findByMember_MemberIdAndActiveFlag(member.getMemberId(), (byte) 1);
+
+        if(routine.isPresent()) throw CustomException.builder()
                 .status(HttpStatus.BAD_REQUEST)
                 .code(RoutineErrorEnum.EXIST_ROUTINE_STATUS.getCode())
                 .message(RoutineErrorEnum.EXIST_ROUTINE_STATUS.getName())
