ebc285480cd52528edc789590e6b20787c63c166
KodaHye <kodhdh09@gmail.com>
2023-08-03 13:43:07 +0900

    chore: 필요없는 주석 삭제

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/service/SchedulerService.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/service/SchedulerService.java
@@ -1,25 +1,15 @@
 package com.ssafy.hellotoday.api.service;
 
-import com.querydsl.core.types.ConstantImpl;
-import com.querydsl.core.types.Expression;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.StringTemplate;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.hellotoday.db.entity.routine.Routine;
-import com.ssafy.hellotoday.db.repository.routine.RoutineCheckRepository;
 import com.ssafy.hellotoday.db.repository.routine.RoutineRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-
-import static com.ssafy.hellotoday.db.entity.routine.QRoutine.routine;
 
 @Service
 @RequiredArgsConstructor
