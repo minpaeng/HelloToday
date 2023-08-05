2018ac4a255002a772042ed75565e3f48f32b7f8
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-08-05 23:49:44 +0900

    feat : dday

--- FE/HelloToday/src/components/Profile/Widget/WidgetDday.js
+++ FE/HelloToday/src/components/Profile/Widget/WidgetDday.js
@@ -1,7 +1,22 @@
+import classess from "./WidgetDday.module.css";
+import { useState, useRef, useCallback } from "react";
+
 function WidgetDday() {
   return (
-    <div className="WidgetDday">
-      <p> 디데이 위젯 </p>
+    <div className={classess.WidgetDday}>
+      <div className={classess.WidgetDday_name}>D-Day</div>
+      <div className={classess.WidgetDday_content}>
+        <div className={classess.WidgetDday_text}>
+          {/* 내 생일 d-30  */}
+          sdfdsfsdfsdf
+        </div>
+        <div className={classess.WidgetDday_edit}>
+          {/* 수정 */}
+          <p className={classess.WidgetDday_txt}>날짜</p>
+          <input type="date" placeholder="날짜를 선택해주세요." />
+          <p className={classess.WidgetDday_txt}>이벤트 입력</p>
+        </div>
+      </div>
     </div>
   );
 }
