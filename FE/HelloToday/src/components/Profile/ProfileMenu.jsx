6322634e2be46747080b937f58c6872ce87222fe
Juahjoah <07juah21@gmail.com>
2023-08-05 17:38:34 +0900

    FEAT: MyProfile Menu function - comment menu rendering

--- FE/HelloToday/src/components/Profile/ProfileMenu.jsx
+++ FE/HelloToday/src/components/Profile/ProfileMenu.jsx
@@ -1,20 +1,141 @@
 import classes from "./ProfileMenu.module.css";
+import { useEffect, useState } from "react";
 
-function ProfileMenu() {
+import ProfileMain from "./ProfileMain";
+import WidgetComments from "./Widget/WidgetComments";
+import WidgetGroupRoutine from "./Widget/WidgetGroupRoutine";
+import WidgetBucket from "./Widget/WidgetBucket";
+import WidgetGoals from "./Widget/WidgetGoals";
+import WidgetDiary from "./Widget/WidgetDiary";
+import WidgetHistory from "./Widget/WidgetHistory";
+import WidgetGallery from "./Widget/WidgetGallery";
+import WidgetDday from "./Widget/WidgetDday";
+
+function ProfileMenu({ setMenu }) {
+  const MenuList = {
+    WidgetComments: <WidgetComments />,
+    WidgetGroupRoutine: <WidgetGroupRoutine />,
+    WidgetBucket: <WidgetBucket />,
+    WidgetGoals: <WidgetGoals />,
+    WidgetDiary: <WidgetDiary />,
+    WidgetHistory: <WidgetHistory />,
+    WidgetGallery: <WidgetGallery />,
+    WidgetDday: <WidgetDday />,
+  };
+
+  const UserSelectMenu = (event) => {
+    console.log(event.target.innerText);
+    setMenu(MenuList[event.target.innerText]);
+  };
+
+  useEffect(() => {
+    setMenu(<WidgetComments />);
+  }, []);
+
   return (
     <div className={classes.ProfileMenu}>
       <h1>ProfileMenu</h1>
-      <p>ProfileMain</p>
-      <p>WidgetGroupRoutine</p> {/* 그룹 루틴 참여 기록*/}
-      <p>WidgetComments</p>
-      <p>WidgetBucket</p>
-      <p>WidgetGoals</p> {/* 일/월/연 목표 */}
-      <p>WidgetDiary</p> {/* 한 줄 일기 */}
-      <p>WidgetHistory</p> {/* 루틴 히스토리 */}
-      <p>WidgetGallery</p> {/* 갤러리 */}
-      <p>WidgetDday</p> {/* 디데이 */}
+
+      <div>
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetGroupRoutine
+        </p>
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetComments
+        </p>
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetBucket
+        </p>
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetGoals
+        </p>
+        {/* 일/월/연 목표 */}
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetDiary
+        </p>
+        {/* 한 줄 일기 */}
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetHistory
+        </p>
+        {/* 루틴 히스토리 */}
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetGallery
+        </p>
+        {/* 갤러리 */}
+        <p
+          onClick={(e) => {
+            UserSelectMenu(e);
+          }}
+        >
+          WidgetDday
+        </p>{" "}
+        {/* 디데이 */}
+      </div>
     </div>
   );
 }
 
 export default ProfileMenu;
+
+// function ProfileMenu() {
+//   const MenuList = [
+//     WidgetComments,
+//     WidgetGroupRoutine,
+//     WidgetBucket,
+//     WidgetGoals,
+//     WidgetDiary,
+//     WidgetHistory,
+//     WidgetGallery,
+//     WidgetDday,
+//   ];
+
+// const ProfileMenuChange = function (props) {
+//   const WidgetComments = <WidgetComments />;
+//   const WidgetGroupRoutine = <WidgetGroupRoutine />;
+//   const WidgetBucket = <WidgetBucket />;
+//   const WidgetGoals = <WidgetGoals />;
+//   const WidgetDiary = <WidgetDiary />;
+//   const WidgetHistory = <WidgetHistory />;
+//   const WidgetGallery = <WidgetGallery />;
+//   const WidgetDday = <WidgetDday />;
+//   return [
+//     WidgetComments,
+//     WidgetGroupRoutine,
+//     WidgetBucket,
+//     WidgetGoals,
+//     WidgetDiary,
+//     WidgetHistory,
+//     WidgetGallery,
+//     WidgetDday,
+//   ];
+// };
+
+// const ProfileMenuChange =
--- FE/HelloToday/src/components/User/config.js
+++ FE/HelloToday/src/components/User/config.js
@@ -1,8 +1,8 @@
 const KAKAO_CLIENT_ID = "342e7bf27eba10f4f52f213866d7146d";
-const KAKAO_REDIRECT_URI = "https://i9b308.p.ssafy.io/login/oauth2/code/kakao";
+// const KAKAO_REDIRECT_URI = "https://i9b308.p.ssafy.io/login/oauth2/code/kakao";
 // const KAKAO_REDIRECT_URI =
 //   "http://i9b308.p.ssafy.io:3000/login/oauth2/code/kakao";
-// const KAKAO_REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
+const KAKAO_REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 const NAVER_CLIENT_ID = "mnX6BbAZD4pm3XzHHq7K";
