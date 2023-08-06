4a47fc3ee3b8aa12153fbd5d734ee63855079175
Juahjoah <07juah21@gmail.com>
2023-08-07 00:39:29 +0900

    INIT: Follow Btn create

---
+++ FE/HelloToday/src/components/Profile/FollowButton.jsx
@@ -0,0 +1,11 @@
+import classes from "./FollowButton.module.css";
+
+function FollowButton() {
+  return (
+    <div className={classes.FollowButton}>
+      <button>Follow</button>
+    </div>
+  );
+}
+
+export default FollowButton;
