cca5ec3aae6b15665f7c2b547fd68a4370979192
권민정 <22alswjd@naver.com>
2023-08-07 10:13:40 +0900

    refactor: question dto 반환으로 수정

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/dto/meetingroom/response/MeetingRoomQuestionResponseDto.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/dto/meetingroom/response/MeetingRoomQuestionResponseDto.java
@@ -1,8 +1,13 @@
 package com.ssafy.hellotoday.api.dto.meetingroom.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@AllArgsConstructor
+@Builder
 @Getter
 public class MeetingRoomQuestionResponseDto {
-
+    private int id;
+    private String content;
 }
