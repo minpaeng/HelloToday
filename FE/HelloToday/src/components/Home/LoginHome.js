d182fd3f635383346f14cc8ba6aef9de64d3c03c
KodaHye <kodhdh09@gmail.com>
2023-08-07 17:01:21 +0900

    feat: 메인 화면 컴포넌트 구성 변경 중

--- FE/HelloToday/src/components/Home/LoginHome.js
+++ FE/HelloToday/src/components/Home/LoginHome.js
@@ -2,25 +2,70 @@
 // flag
 //  - 0: 진행 루틴 있을 때의 컴포넌트
 //  - 1: 진행 루틴 없을 때의 컴포넌트
-
+import MainBanner from "../common/MainBanner";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Nav from "../../components/common/Nav";
+import { useSelector } from "react-redux";
+import UnSelectedRoutine from "../../components/routine/UnSeletedRoutine";
+import SelectedRoutine from "../routine/SelectedRoutine";
 
 function LoginHome() {
-    useEffect(() => {
-        async function axiosPrivateRoutineData() {
-            try {
-                const privateRoutineResponse = await axios.get(
+  const LOCAL_URL = "http://localhost:8080";
+  const [routinePrivateResponse, setRoutinePrivate] = useState([]);
+  const AccsesToken = useSelector((state) => state.authToken.accessToken);
+  const routineSelectBannerImg = "main_banner_routineselect1";
+  const routineSelectMainBannerMents = [
+    "쉽지 않은 생활 습관 만들기",
+    "계획을 떠나 아예 뭘 해야할지 모르겠다구요?",
+    "결심했다는 마음이 중요한거예요 :)",
+  ];
 
-                )
-            } catch (error) {
-                console.log("Error fetching data: ", error);
-            }
-        }
-    }, []);
-    return (
-        <div></div>
-    );
+  const [routineDetailCheckList, setRoutineDetailCheckList] = useState();
+
+  useEffect(() => {
+    async function axiosRoutinePrivateData() {
+      try {
+        const routinePrivateResponse = await 
+        axios.get(`${LOCAL_URL}/api/routine/private`, {
+            headers: {
+              Authorization: AccsesToken,
+            }}
+        );
+
+        setRoutinePrivate(routinePrivateResponse.data);
+
+        // 현재 진행하고 있는 루틴 리스트(인증 내역)
+        // const routineDetailCheckList = routinePrivateResponse.data.routineDetailCatCheck;
+        setRoutineDetailCheckList(routinePrivateResponse.data.routineDetailCatCheck)
+        console.log(">>")
+        console.log(routineDetailCheckList)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    axiosRoutinePrivateData();
+  }, []);
+
+  return (
+
+    <div>
+      <MainBanner
+        bannerImg={routineSelectBannerImg}
+        bannerMent={routineSelectMainBannerMents}
+      />
+      <Nav />
+
+      {routinePrivateResponse.activeFlag === 1 ? (
+        // 진행 중인 루틴이 있는 경우
+        <SelectedRoutine routineCheckList={routineDetailCheckList}/>
+      ) : (
+        // 진행 중인 루틴이 없는 경우
+        <UnSelectedRoutine />
+      )}
+
+    </div>
+  );
 }
 
 export default LoginHome;
--- FE/HelloToday/src/pages/Home/Home.js
+++ FE/HelloToday/src/pages/Home/Home.js
@@ -1,19 +1,28 @@
 import classes from "./Home.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import NoLoginHome from "../../components/Home/NoLoginHome";
 import LoginHome from "../../components/Home/LoginHome";
 import Footer from "../../components/common/Footer";
+import { useEffect } from "react";
+import allAuth from "../../components/User/allAuth";
 
 function Home() {
 
   const islogin = useSelector((state) => state.login.isLogin);
   console.log("로그인 상태 >: " + islogin)
+    //------------------------------로그인 시작
+    const dispatch = useDispatch();
+    const isAccess = useSelector((state) => state.authToken.accessToken);
+    useEffect(() => {
+      allAuth(isAccess, dispatch);
+    }, [dispatch]);
+    //-----------------------------------여기까지
 
   return (
     <div className={classes.HomeMain}>
 
       {/* 로그인 여부에 따라서 컴포넌트 렌더링 */}
-      {islogin ? <LoginHome/> : <NoLoginHome/>}
+      { islogin? <LoginHome/> : <NoLoginHome/>}
 
       <Footer/>
     </div>
