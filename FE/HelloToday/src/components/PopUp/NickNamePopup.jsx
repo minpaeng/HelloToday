45e450b3e824be6df3efe356e046ea987b7a956e
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-08-07 15:51:32 +0900

    develop merge

--- FE/HelloToday/src/components/PopUp/NickNamePopup.jsx
+++ FE/HelloToday/src/components/PopUp/NickNamePopup.jsx
@@ -7,8 +7,6 @@ import classNames from "classnames";
 function NickNamePopup({ FirstLogin, setFirstLogin, Token, setNickName }) {
   // // const API_URL = "https://i9b308.p.ssafy.io";
   const API_URL = "http://localhost:8080";
-
-  const API_URL = "http://localhost:8080";
   const [userName, setUserName] = useState("");
   // 정규식 통과 검사(닉네임 형식)
   const [isUserName, setIsUserName] = useState(false);
