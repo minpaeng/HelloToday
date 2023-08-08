4672085ddc23a39ff87e7561bd0d25e2270b9866
yoondonghun <acttoze1@gmail.com>
2023-08-08 17:16:05 +0900

    fix: timer fix

--- FE/HelloToday/src/components/video/LeftsideRoomInfoTime.jsx
+++ FE/HelloToday/src/components/video/LeftsideRoomInfoTime.jsx
@@ -5,18 +5,15 @@ function LeftsideRoomInfoTime() {
   const [elapsedTime, setElapsedTime] = useState(0);
 
   useEffect(() => {
-    // 1초마다 elapsed time 증가
     const interval = setInterval(() => {
       setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
     }, 1000);
 
     return () => {
-      // 컴포넌트가 unmount될 때 interval 정리
       clearInterval(interval);
     };
   }, []);
 
-  // 분:초 형식으로 변환하는 함수
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
