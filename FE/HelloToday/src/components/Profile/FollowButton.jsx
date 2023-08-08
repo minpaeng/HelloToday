902a375b314bc2e4bc61981a2c0efbab40647eaf
Juahjoah <07juah21@gmail.com>
2023-08-08 23:58:03 +0900

    FIX: Follow Btn error - axios error - button innertext

--- FE/HelloToday/src/components/Profile/FollowButton.jsx
+++ FE/HelloToday/src/components/Profile/FollowButton.jsx
@@ -4,15 +4,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-const baseURL = "https://i9b308.p.ssafy.io"; // 배포용으로 보내면, 아직 확인불가(develop에서만 확인가능)
-// const baseURL = "http://localhost:8080"; // 개발용
+// const baseURL = "https://i9b308.p.ssafy.io"; // 배포용으로 보내면, 아직 확인불가(develop에서만 확인가능)
+const baseURL = "http://localhost:8080"; // 개발용
 
 // props로 유저 정보 받아오기, 아예 처음부터 팔로잉 정보도 받아올까?
 function FollowButton(props) {
   const AccsesToken = useSelector((state) => state.authToken.accessToken);
   // const UserId = sessionStorage.getItem("memberId");
-  const [isFollow, setIsFollow] = useState();
-  // 들어온 프로필이 나인가, 아닌가
+  // 팔로잉 상태를 구분하는 변수
+  const [isFollow, setIsFollow] = useState(false);
+  // 접속한 프로필이 나인가, 아닌가 판단해서 값을 주기
   const [isMe, setIsMe] = useState(false);
 
   // 시작할때, 팔로우 정보 가져와서 담아두기!
@@ -22,44 +23,63 @@ function FollowButton(props) {
     );
 
     axios
-      .get(
-        `${baseURL}/api/follow/`,
-        { params: { memberId: props.memberId } },
-        { Authorization: AccsesToken }
-      )
+      .get(`${baseURL}/api/follow/`, {
+        params: { memberId: props.memberId },
+        headers: { Authorization: AccsesToken },
+      })
       .then((response) => {
-        // console.log(response.data);
-        // console.log(response.success);
-        setIsFollow(response.success);
+        if (response.data.success === true) {
+          setIsFollow(true);
+        } else {
+          setIsFollow(false);
+        }
+        console.log(response.data.success);
+        // setIsFollow(response.data.success);
+        console.log(isFollow);
       })
       .catch((error) => {
-        // console.log(error);
+        console.log(error);
       });
   }, [props.memberId]);
 
-  const UserFollow = () => {
-    axios
-      .request({
-        method: isFollow ? "delete" : "post",
-        url: `${baseURL}/api/follow`,
-        data: {
+  const UserFollowClick = (event) => {
+    if (isFollow) {
+      event.target.innerText = "unFollow";
+      axios
+        .post(
+          `${baseURL}/api/follow`,
+          { followingId: props.memberId },
+          {
+            headers: { Authorization: AccsesToken },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          setIsFollow((isFollow) => !isFollow);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      event.target.innerText = "Follow";
+      axios
+        .delete(`${baseURL}/api/follow`, {
+          params: { target: props.memberId },
           headers: { Authorization: AccsesToken },
-          body: { followingId: props.memberId },
-          // 'Content-type': 'application/json',
-        },
-      })
-      .then((response) => {
-        // console.log(response.data);
-        setIsFollow((isFollow) => !isFollow);
-      })
-      .catch((error) => {
-        // console.log(error);
-      });
+        })
+        .then((response) => {
+          console.log(response.data);
+          setIsFollow((isFollow) => !isFollow);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   return (
     <div className={classes.FollowButton}>
-      {!isMe ? <button onClick={UserFollow}>Follow</button> : null}
+      {!isMe ? <button onClick={UserFollowClick}>Follow</button> : null}
       <button onClick={() => props.setFollowButtonClick(true)}>
         팔로우하는 사람보기
       </button>
