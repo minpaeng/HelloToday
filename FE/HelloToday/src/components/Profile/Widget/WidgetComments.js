85925f62c38549de798e331f3f5ad93edfcfae03
Juahjoah <07juah21@gmail.com>
2023-08-09 10:55:31 +0900

    FEAT: create Comment Widget - Get, Post fucntion

--- FE/HelloToday/src/components/Profile/Widget/WidgetComments.js
+++ FE/HelloToday/src/components/Profile/Widget/WidgetComments.js
@@ -1,7 +1,149 @@
-function WidgetComments() {
+import classes from "./WidgetComments.module.css";
+
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
+// const baseURL = "https://i9b308.p.ssafy.io"; // 배포용으로 보내면, 아직 확인불가(develop에서만 확인가능)
+const baseURL = "http://localhost:8080"; // 개발용
+
+function WidgetComments(props) {
+  const AccsesToken = useSelector((state) => state.authToken.accessToken);
+  const memberId = props.memberId;
+
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+
+  const page = 0;
+  const size = 10;
+
+  const getComments = (memberId) => {
+    axios
+      .get(
+        `${baseURL}/api/mypage/cheermsg/${memberId}`,
+        {
+          params: { memberId: memberId, page: page, size: size },
+        },
+        {
+          headers: { Authorization: AccsesToken },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        setComments(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getComments(memberId);
+  }, [memberId]);
+
+  const CreateComment = () => {
+    axios
+      .post(
+        `${baseURL}/api/mypage/cheermsg`,
+        {
+          memberId,
+          content: newComment,
+        },
+        {
+          headers: { Authorization: AccsesToken },
+        }
+      )
+      .then((response) => {
+        // console.log(response.data);
+        // if (response.data.length === 0) {
+        //   alert("내용을 입력해주세요.");
+        // } else {
+        //   alert("응원 메시지가 등록되었습니다.");
+        setNewComment("");
+        getComments();
+        // }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const EditComment = (messageId, editedContent) => {
+    axios
+      .put(
+        `${baseURL}/api/comments/${messageId}`,
+        {
+          content: editedContent,
+        },
+        {
+          headers: { Authorization: AccsesToken },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        // 댓글 수정 후 필요한 업데이트 로직 구현
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const DeleteComment = (messageId) => {
+    axios
+      .delete(`${baseURL}/api/comments/${messageId}`, {
+        // params: { cheerMessageId: messageId },
+        headers: { Authorization: AccsesToken },
+      })
+      .then((response) => {
+        console.log(response.data);
+        // 댓글 삭제 후 필요한 업데이트 로직 구현
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="WidgetComments">
-      <p> 응원 댓글</p>
+    <div className={classes.WidgetComments}>
+      <div>
+        <p> {memberId}님의 응원 댓글</p>
+        <div className={classes.CommentSection}>
+          {comments.map((comment) => (
+            // <div key={comment.id}>
+            <div key={comment.messageId}>
+              {comment.content}
+
+              <button className={classes.buttonstyle}>
+                <img
+                  className={classes.edit}
+                  src="../../images/Widget/edit.png"
+                  alt="edit"
+                  onClick={() => EditComment()}
+                />
+              </button>
+              <button className={classes.buttonstyle}>
+                <img
+                  className={classes.clear}
+                  src="../../images/Widget/clear.png"
+                  alt="clear"
+                  onClick={() => DeleteComment(comment.messageId)}
+                />
+              </button>
+            </div>
+          ))}
+          <div className={classes.btn_edit_delete}>
+            {/* 해당 이미지 부분 CSS는 DDay 위젯에서 총괄 관리 */}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          value={newComment}
+          onChange={(event) => setNewComment(event.target.value)}
+        />
+        <button onClick={CreateComment}>댓글 작성</button>
+      </div>
     </div>
   );
 }
