e77b2d8687cb1691908a48dc9e62b6845277aede
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-08-10 10:08:10 +0900

    feat: routine 히스토리 구현 중

--- FE/HelloToday/src/components/Profile/Widget/WidgetHistory.js
+++ FE/HelloToday/src/components/Profile/Widget/WidgetHistory.js
@@ -1,7 +1,43 @@
+import classes from "./WidgetHistory.module.css";
+import { useState, useRef, useCallback, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+
 function WidgetHistory() {
+  const memberId = useParams().memberId; //url에서 param가져오기
+  const [data, setData] = useState([]);
+  const AccsesToken = useSelector((state) => state.authToken.accessToken);
+  const [curpage, setCurPage] = useState("1");
+
+  useEffect(() => {
+    const params = {
+      memberId: memberId,
+      page: curpage,
+      size: 3,
+    };
+    const headers = {
+      Authorization: AccsesToken,
+    };
+    axios
+      .get(
+        `${process.env.REACT_APP_BASE_URL}/api/mypage/routinehistory/${memberId}`,
+        { params, headers }
+      )
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
     <div className="widget-history">
-      <p> 루틴 히스토리 </p>
+      <div className={classes.WidgetHistory}>
+        <span className={classes.WidgetHistory_name}>루틴 히스토리</span>
+        <div className={classes.WidgetHistory_content}></div>
+      </div>
     </div>
   );
 }
