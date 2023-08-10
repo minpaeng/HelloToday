9229f41497bf49548603ae1175e2804b9c303a60
권민정 <22alswjd@naver.com>
2023-08-10 09:42:15 +0900

    style: 코드 스타일 정리, security 설정

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/config/WebConfig.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.ssafy.hellotoday.config;
 
 import com.ssafy.hellotoday.common.util.property.ApplicationProperties;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -15,14 +14,18 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .exposedHeaders("Authorization", "Access-Token","Authorization-Refresh","Refresh-Token")
+                .exposedHeaders(
+                        "Authorization", "Access-Token",
+                        "Authorization-Refresh", "Refresh-Token")
                 .maxAge(3600);
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/profile/**","/routine/**")
-                .addResourceLocations("file:///" + ApplicationProperties.PROFILE_PATH,"file:///" + ApplicationProperties.ROUTINECHECK_PATH)
+        registry.addResourceHandler("/profile/**", "/routine/**")
+                .addResourceLocations(
+                        "file:///" + ApplicationProperties.PROFILE_PATH,
+                        "file:///" + ApplicationProperties.ROUTINECHECK_PATH)
                 .setCachePeriod(60 * 10)
                 .resourceChain(true);
     }
