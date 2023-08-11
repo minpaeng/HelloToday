f3ea9a5d3328f8a29c657adb68e29e0e692161f3
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-08-11 15:44:19 +0900

    design:email 동의 말 추가

--- FE/HelloToday/src/components/Login/Spinner.js
+++ FE/HelloToday/src/components/Login/Spinner.js
@@ -1,10 +1,16 @@
 import React from "react";
-import FadeLoader from "react-spinners/FadeLoader";
+import HashLoader from "react-spinners/HashLoader";
+import classes from "./Spinners.module.css";
 
 function Spinner() {
   return (
-    <div>
-      <FadeLoader color="#ccbbff" size={40} />
+    <div className={classes.spinner_container}>
+      <div className={classes.spinner_location}>
+        <HashLoader color="#ffffff" size={300} />
+      </div>
+      <div>
+        <p className={classes.spinner_txt}>잠시만 기다려 주세요</p>
+      </div>
     </div>
   );
 }
