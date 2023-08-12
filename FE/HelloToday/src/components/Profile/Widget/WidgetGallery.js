95331c88b498d95acad4cd4c7face978070d3a4a
김주아 <07juahlily21@naver.com>
2023-08-12 21:12:36 +0900

    FEAT: create widget gallery function

--- FE/HelloToday/src/components/Profile/Widget/WidgetGallery.js
+++ FE/HelloToday/src/components/Profile/Widget/WidgetGallery.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 function WidgetGallery() {
+  const AccsesToken = useSelector((state) => state.authToken.accessToken);
+  const memberId = useParams().memberId;
+
+  const [gallery, setGallery] = useState([]);
+
+  const getGallery = () => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/api/mypage/gallery/${memberId}`, {
+        headers: { Authorization: AccsesToken },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setGallery(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {});
+
   return (
     <div className="WidgetGallery">
-      <p> 갤러리인데, 다른 메뉴로 변경하기로 함</p>
+      <p> 소중한 루틴 추억 ^_^</p>
     </div>
   );
 }
