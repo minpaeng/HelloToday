dd1007f85f176f911c769abd2900442d064b712a
김주아 <07juahlily21@naver.com>
2023-08-12 21:25:26 +0900

    FIT: error check

--- FE/HelloToday/src/components/Profile/Widget/WidgetBucket.js
+++ FE/HelloToday/src/components/Profile/Widget/WidgetBucket.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+
 import axios from "axios";
 
 function WidgetBucket() {
