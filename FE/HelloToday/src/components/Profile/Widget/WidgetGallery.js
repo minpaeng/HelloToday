1459d7489cbd430ec81ddd72c49c90187737e503
김주아 <07juahlily21@naver.com>
2023-08-12 21:48:18 +0900

    FEAT: create widget gallery function

--- FE/HelloToday/src/components/Profile/Widget/WidgetGallery.js
+++ FE/HelloToday/src/components/Profile/Widget/WidgetGallery.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+
 import axios from "axios";
 
 function WidgetGallery() {
@@ -23,11 +24,23 @@ function WidgetGallery() {
       });
   };
 
-  useEffect(() => {});
+  useEffect(() => {
+    getGallery();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [memberId, AccsesToken]);
 
   return (
     <div className="WidgetGallery">
       <p> 소중한 루틴 추억 ^_^</p>
+      <div>
+        {gallery.length === 0 && <p>등록된 사진이 없습니다.</p>}
+        {gallery.length > 0 &&
+          gallery.map((galleryItem) => (
+            <div key={galleryItem.imgPath}>
+              <img src={galleryItem.imgPath} alt="userGallery.Item" />
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
