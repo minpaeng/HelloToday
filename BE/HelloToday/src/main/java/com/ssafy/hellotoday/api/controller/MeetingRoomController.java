c99d9353908ecf575556f2c891781553d0e4a9f0
권민정 <22alswjd@naver.com>
2023-08-13 15:55:40 +0900

    refactor: member 인증 정보 가져와서 사용하도록 리펙토링

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/controller/MeetingRoomController.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/controller/MeetingRoomController.java
@@ -5,13 +5,12 @@ import com.ssafy.hellotoday.api.dto.meetingroom.request.RoomCreateRequestDto;
 import com.ssafy.hellotoday.api.dto.meetingroom.response.MeetingRoomPageDto;
 import com.ssafy.hellotoday.api.dto.meetingroom.response.RoomCreateResponseDto;
 import com.ssafy.hellotoday.api.service.MeetingRoomService;
-import com.ssafy.hellotoday.api.service.MemberService;
 import com.ssafy.hellotoday.api.service.OpenviduService;
-import com.ssafy.hellotoday.common.exception.CustomException;
-import com.ssafy.hellotoday.db.entity.Member;
+import com.ssafy.hellotoday.jwt.MemberDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Tag(name = "Meeting Room", description = "미팅룸 관련 API")
 @RequiredArgsConstructor
 @RestController
@@ -30,7 +27,6 @@ import javax.servlet.http.HttpServletRequest;
 public class MeetingRoomController {
     private final OpenviduService openviduService;
     private final MeetingRoomService meetingRoomService;
-    private final MemberService memberService;
 
     /**
      * 미팅룸을 생성하고 참여하기 위한 세션 아이디, 토큰을 반환
@@ -42,17 +38,14 @@ public class MeetingRoomController {
             description = "미팅룸을 생성하고 참여하기 위한 세션 아이디, 토큰을 반환받는 API")
     @PostMapping
     public BaseResponseDto createRoom(@RequestBody(required = false) RoomCreateRequestDto requestDto,
-                                      HttpServletRequest httpServletRequest) {
-
-        String token = httpServletRequest.getHeader("Authorization");
-        Member member = memberService.findMemberByJwtToken(token);
+                                      @AuthenticationPrincipal MemberDetailsImpl member) {
 
         if (requestDto == null) requestDto = new RoomCreateRequestDto();
         BaseResponseDto responseDto = openviduService.createRoom(requestDto);
 
         String sessionId = ((RoomCreateResponseDto) responseDto.getData()).getSessionId();
         ((RoomCreateResponseDto) responseDto.getData())
-                .setRoomId(meetingRoomService.saveRoomInfo(member, requestDto, sessionId));
+                .setRoomId(meetingRoomService.saveRoomInfo(member.getUser(), requestDto, sessionId));
 
         return responseDto;
     }
@@ -67,11 +60,7 @@ public class MeetingRoomController {
             summary = "미팅룸 참여",
             description = "미팅룸에 참여하기 위한 토큰을 반환받는 API")
     @GetMapping("/{roomId}/connections")
-    public BaseResponseDto joinRoom(@PathVariable("roomId") int roomId,
-                                    HttpServletRequest httpServletRequest) {
-        String token = httpServletRequest.getHeader("Authorization");
-        Member member = memberService.findMemberByJwtToken(token);
-        if (member == null) throw CustomException.builder().code(1000).message("사용자를 찾을 수 없음").build();
+    public BaseResponseDto joinRoom(@PathVariable("roomId") int roomId) {
 
         return openviduService.joinRoom(roomId);
     }
@@ -80,12 +69,8 @@ public class MeetingRoomController {
             summary = "미팅룸 목록 조회",
             description = "현재 개설되어 있는 미팅룸 목록을 조회하는 API")
     @GetMapping("/list")
-    public MeetingRoomPageDto roomList(HttpServletRequest httpServletRequest,
-                                       @RequestParam(required = false, defaultValue = "1", value = "page")int page,
+    public MeetingRoomPageDto roomList(@RequestParam(required = false, defaultValue = "1", value = "page")int page,
                                        @RequestParam(required = false, defaultValue = "6", value = "size")int size) {
-        String token = httpServletRequest.getHeader("Authorization");
-        Member member = memberService.findMemberByJwtToken(token);
-        if (member == null) throw CustomException.builder().code(1000).message("사용자를 찾을 수 없음").build();
 
         return openviduService.roomList(page - 1, size);
     }
@@ -94,11 +79,7 @@ public class MeetingRoomController {
             summary = "미팅룸 퇴장",
             description = "미팅룸을 퇴장하고, 마지막 사람이 퇴장했을 때 미팅룸 세션을 닫는 API")
     @DeleteMapping("/{roomId}")
-    public BaseResponseDto deleteConnection(@PathVariable("roomId") int roomId,
-                                            HttpServletRequest httpServletRequest) {
-        String token = httpServletRequest.getHeader("Authorization");
-        Member member = memberService.findMemberByJwtToken(token);
-        if (member == null) throw CustomException.builder().code(1000).message("사용자를 찾을 수 없음").build();
+    public BaseResponseDto deleteConnection(@PathVariable("roomId") int roomId) {
 
         return openviduService.deleteConnection(roomId);
     }
@@ -107,11 +88,7 @@ public class MeetingRoomController {
             summary = "미팅룸에서 루틴 진행 도움 질문 조회",
             description = "미티룸에서 루틴 진행에 도움이 되는 멘트를 조회하는 API")
     @GetMapping("/{roomId}/question")
-    public BaseResponseDto getQuestion(@PathVariable("roomId") int roomId,
-                                       HttpServletRequest httpServletRequest) {
-        String token = httpServletRequest.getHeader("Authorization");
-        Member member = memberService.findMemberByJwtToken(token);
-        if (member == null) throw CustomException.builder().code(1000).message("사용자를 찾을 수 없음").build();
+    public BaseResponseDto getQuestion(@PathVariable("roomId") int roomId) {
 
         return meetingRoomService.getQuestion(roomId);
     }
