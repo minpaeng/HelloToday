5f3ea76c717b97db40c26b17da467857b65a0e5f
yoondonghun <acttoze1@gmail.com>
2023-08-13 18:00:49 +0900

    fix : login Home

--- FE/HelloToday/src/components/Home/LoginHome.js
+++ FE/HelloToday/src/components/Home/LoginHome.js
@@ -9,7 +9,7 @@ import UnSelectedRoutine from "../routine/UnSelectedRoutine";
 import SelectedRoutine from "../routine/SelectedRoutine";
 import Footer from "../common/Footer";
 import { useDispatch, useSelector } from "react-redux";
-import { routineCheck } from "../../store/routineCheckModalSlice"
+import { routineCheck } from "../../store/routineCheckModalSlice";
 
 function LoginHome() {
   const dispatch = useDispatch();
@@ -18,10 +18,9 @@ function LoginHome() {
 
   const haveActiveRoutine = useSelector((state) => state.haveActiveRoutine); // 추가
   const routineCheckFlag = useSelector((state) => state.routineCheck);
-  
+
   useEffect(() => {
     async function axiosRoutinePrivateData() {
-
       try {
         const routinePrivateResponse = await axios.get(
           `${process.env.REACT_APP_BASE_URL}/api/routine/private`,
@@ -33,7 +32,6 @@ function LoginHome() {
         );
 
         setRoutinePrivate(routinePrivateResponse.data);
-
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -42,15 +40,9 @@ function LoginHome() {
     const delay = 200;
     const timerId = setTimeout(axiosRoutinePrivateData, delay);
     dispatch(routineCheck(false));
-    
+
     return () => clearTimeout(timerId);
   }, [haveActiveRoutine, routineCheckFlag]);
-
-    const delay = 200;
-    const timerId = setTimeout(axiosRoutinePrivateData, delay);
-
-    return () => clearTimeout(timerId);
-  }, [haveActiveRoutine]);
 
   return (
     <div>
