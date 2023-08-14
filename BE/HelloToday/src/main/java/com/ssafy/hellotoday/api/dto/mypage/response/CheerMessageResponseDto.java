119fe8efa3b1005222eca359e9e0e3e90d2c2238
KodaHye <kodhdh09@gmail.com>
2023-08-15 00:50:09 +0900

    fix: 응원메시지에 작성자 ID 추가

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/dto/mypage/response/CheerMessageResponseDto.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/dto/mypage/response/CheerMessageResponseDto.java
@@ -10,15 +10,17 @@ import java.time.LocalDateTime;
 public class CheerMessageResponseDto {
     private Integer messageId;
     private String writerNickName;
+    private Integer writerId;
     private Integer memberId;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
     private String content;
 
     @Builder
-    public CheerMessageResponseDto(Integer messageId, String writerNickName, Integer memberId, LocalDateTime createdDate, LocalDateTime modifiedDate, String content) {
+    public CheerMessageResponseDto(Integer messageId, String writerNickName, Integer writerId, Integer memberId, LocalDateTime createdDate, LocalDateTime modifiedDate, String content) {
         this.messageId = messageId;
         this.writerNickName = writerNickName;
+        this.writerId = writerId;
         this.memberId = memberId;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
@@ -28,6 +30,7 @@ public class CheerMessageResponseDto {
     public CheerMessageResponseDto(CheerMessage cheerMessage) {
         this.messageId = cheerMessage.getCheerMessageId();
         this.writerNickName = cheerMessage.getWriter().getNickname();
+        this.writerId = cheerMessage.getWriter().getMemberId();
         this.memberId = cheerMessage.getMember().getMemberId();
         this.createdDate = cheerMessage.getCreatedDate();
         this.modifiedDate = cheerMessage.getModifiedDate();
