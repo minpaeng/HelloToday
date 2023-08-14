348450372824c0701452c7ed07e2304b5036d719
김주아 <07juahlily21@naver.com>
2023-08-14 14:08:58 +0900

    FEAT: follow btn clicked profile

--- FE/HelloToday/src/components/Profile/FollowList.jsx
+++ FE/HelloToday/src/components/Profile/FollowList.jsx
@@ -2,9 +2,10 @@ import classes from "./FollowList.module.css";
 
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import axios from "axios";
+import { set } from "lodash";
 
 function FollowList() {
   const AccsesToken = useSelector((state) => state.authToken.accessToken);
@@ -16,9 +17,12 @@ function FollowList() {
   const [nowPage, setNowPage] = useState(1);
   const itemsIncludePage = 5;
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     getUserFollowers(memberId);
     getUserFollowings(memberId);
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [memberId, AccsesToken]);
 
@@ -52,6 +56,7 @@ function FollowList() {
       });
   };
 
+  // 페이지네이션
   const indexOfLastItem = nowPage * itemsIncludePage;
   const indexOfFirstItem = indexOfLastItem - itemsIncludePage;
   const nowFollowings = Followings.slice(indexOfFirstItem, indexOfLastItem);
@@ -61,14 +66,38 @@ function FollowList() {
     setNowPage(pageNumber);
   };
 
+  // 팔로잉, 팔로워 리스트 프로필 연결
+
+  const FollowCardClick = (memberId) => {
+    // navigate(`${process.env.REACT_APP_BASE_URL}/api/mypage/${memberId}`);
+    navigate(`/MyProfile/${memberId}/`);
+    setTimeout(() => {
+      window.location.reload(); // 차후에 변경하기
+    }, 10);
+  };
+
   return (
     <div>
       <h1>함께하는 모듈러 보기</h1>
       <h3>내가 팔로잉하는 사람</h3>
+      {Followings.length > itemsIncludePage && (
+        <div>
+          <button
+            onClick={() => paginate(nowPage - 1)}
+            disabled={nowPage === 1}
+          >
+            <img src="../../images/Widget/before.png" alt="before" />
+          </button>
+        </div>
+      )}
       {nowFollowings.length === 0 && <div>팔로잉 없음</div>}
       {nowFollowings.length > 0 &&
         nowFollowings.map((following) => (
-          <div className={classes.followItem} key={following.memberId}>
+          <div
+            className={classes.followCard}
+            key={following.memberId}
+            onClick={() => FollowCardClick(following.memberId)}
+          >
             <img
               className={classes.followImg}
               src={following.profilePath}
@@ -80,28 +109,38 @@ function FollowList() {
       {Followings.length > itemsIncludePage && (
         <div>
           <button
-            onClick={() => paginate(nowPage - 1)}
-            disabled={nowPage === 1}
-          >
-            이전
-          </button>
-          <button
             onClick={() => paginate(nowPage + 1)}
             disabled={
               nowFollowings.length < itemsIncludePage ||
               nowFollowings.length === 0
             }
           >
-            다음
+            <img src="../../images/Widget/next.png" alt="next" />
           </button>
         </div>
       )}
 
       <h3>나를 팔로우하는 사람</h3>
+
+      {Followers.length > itemsIncludePage && (
+        <div>
+          <button
+            onClick={() => paginate(nowPage - 1)}
+            disabled={nowPage === 1}
+          >
+            <img src="../../images/Widget/before.png" alt="before" />
+          </button>
+        </div>
+      )}
+
       {nowFollowers.length === 0 && <div>팔로워 없음</div>}
       {nowFollowers.length > 0 &&
         nowFollowers.map((follower) => (
-          <div className={classes.followItem} key={follower.memberId}>
+          <div
+            className={classes.followCard}
+            key={follower.memberId}
+            onClick={() => FollowCardClick(follower.memberId)}
+          >
             <img
               className={classes.followImg}
               src={follower.profilePath}
@@ -113,19 +152,13 @@ function FollowList() {
       {Followers.length > itemsIncludePage && (
         <div>
           <button
-            onClick={() => paginate(nowPage - 1)}
-            disabled={nowPage === 1}
-          >
-            이전
-          </button>
-          <button
             onClick={() => paginate(nowPage + 1)}
             disabled={
               nowFollowers.length < itemsIncludePage ||
               nowFollowers.length === 0
             }
           >
-            다음
+            <img src="../../images/Widget/next.png" alt="next" />
           </button>
         </div>
       )}
