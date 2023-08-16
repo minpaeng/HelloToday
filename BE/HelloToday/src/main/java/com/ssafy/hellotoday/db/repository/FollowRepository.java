29494531144dbf4036ffbbdd9a3aa3871200d77f
권민정 <22alswjd@naver.com>
2023-08-16 15:23:32 +0900

    feat: 팔로워, 팔로잉 조회 페이징 처리

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/db/repository/FollowRepository.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/db/repository/FollowRepository.java
@@ -2,18 +2,30 @@ package com.ssafy.hellotoday.db.repository;
 
 import com.ssafy.hellotoday.db.entity.Follow;
 import com.ssafy.hellotoday.db.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
     Optional<Follow> findByFollowerAndFollowing(Member follower, Member following);
 
-    @Query("select f from Follow f join fetch f.follower where f.following.memberId = :memberId")
-    List<Follow> findAllByFollowing(int memberId);
+    @Query(
+            value = "select f " +
+                    "from Follow f " +
+                    "join fetch f.follower " +
+                    "where f.following.memberId = :memberId " +
+                    "order by f.follower.nickname",
+            countQuery = "select f from Follow f where f.following.memberId = :memberId")
+    Page<Follow> findAllByFollowing(int memberId, Pageable pageable);
 
-    @Query("select f from Follow f join fetch f.following where f.follower.memberId = :memberId")
-    List<Follow> findAllByFollower(int memberId);
+    @Query(
+            value = "select f " +
+                    "from Follow f " +
+                    "join fetch f.following " +
+                    "where f.follower.memberId = :memberId order by f.following.nickname",
+            countQuery = "select f from Follow f where f.follower.memberId = :memberId")
+    Page<Follow> findAllByFollower(int memberId, Pageable pageable);
 }
