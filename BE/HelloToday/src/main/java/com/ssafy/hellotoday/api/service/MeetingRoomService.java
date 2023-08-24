e53eff9201b6407daf071a9a9f8e995b0eed2d13
권민정 <22alswjd@naver.com>
2023-08-24 20:06:27 +0900

    refactor: 미팅룸 질문 조회 리팩토링

--- BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/service/MeetingRoomService.java
+++ BE/HelloToday/src/main/java/com/ssafy/hellotoday/api/service/MeetingRoomService.java
@@ -5,6 +5,7 @@ import com.ssafy.hellotoday.api.dto.meetingroom.request.RoomCreateRequestDto;
 import com.ssafy.hellotoday.api.dto.meetingroom.response.MeetingRoomQuestionResponseDto;
 import com.ssafy.hellotoday.common.exception.CustomException;
 import com.ssafy.hellotoday.common.exception.message.MeetingRoomErrorEnum;
+import com.ssafy.hellotoday.common.exception.validator.MeetingRoomValidator;
 import com.ssafy.hellotoday.common.util.constant.MeetingRoomResponseEnum;
 import com.ssafy.hellotoday.db.entity.MeetingRoom;
 import com.ssafy.hellotoday.db.entity.MeetingRoomQuestion;
@@ -23,6 +24,8 @@ import java.util.Optional;
 public class MeetingRoomService {
     private final MeetingRoomRepository meetingRoomRepository;
     private final MeetingRoomQuestionRepository meetingRoomQuestionRepository;
+
+    private final MeetingRoomValidator meetingRoomValidator;
 
     @Transactional
     public int saveRoomInfo(Member member, RoomCreateRequestDto requestDto, String sessionId) {
@@ -43,29 +46,16 @@ public class MeetingRoomService {
 
     @Transactional
     public BaseResponseDto getQuestion(int roomId) {
-        MeetingRoom room = meetingRoomRepository.findByIdWithQuestionId(roomId)
-                .orElseThrow(() -> CustomException.builder()
-                        .status(HttpStatus.BAD_REQUEST)
-                        .code(MeetingRoomErrorEnum.ROOM_NOT_EXIST.getCode())
-                        .message(MeetingRoomErrorEnum.ROOM_NOT_EXIST.getMessage())
-                        .build());
+        Optional<MeetingRoom> roomOptional = meetingRoomRepository.findByIdWithQuestionId(roomId);
+        MeetingRoom room = meetingRoomValidator.validMeetingRoom(roomOptional);
 
-        long count = meetingRoomQuestionRepository.count();
-        int questionId = room.getQuestion().getQuestionId();
-        System.out.println(questionId);
-        if (questionId >= count) questionId = 0;
-        questionId++;
-        MeetingRoomQuestion question = meetingRoomQuestionRepository.findById(questionId)
-                .orElseThrow(() -> CustomException.builder()
-                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .code(MeetingRoomErrorEnum.FAILED_MEETINGROOM_QUESTION.getCode())
-                        .message(MeetingRoomErrorEnum.FAILED_MEETINGROOM_QUESTION.getMessage())
-                        .build());
+        int questionId = getQuestionId(room);
 
-        System.out.println(MeetingRoomQuestionResponseDto.builder()
-                .id(question.getQuestionId())
-                .content(question.getContent()));
+        Optional<MeetingRoomQuestion> questionOptional = meetingRoomQuestionRepository.findById(questionId);
+        MeetingRoomQuestion question = meetingRoomValidator.validateMeetingRoomQuestion(questionOptional);
+
         room.updateQuestion(question);
+
         return BaseResponseDto.builder()
                 .success(true)
                 .message(MeetingRoomResponseEnum.SUCCESS_MEETINGROOM_QUESTION.getName())
@@ -74,6 +64,14 @@ public class MeetingRoomService {
                         .content(question.getContent())
                         .build())
                 .build();
+    }
+
+    private int getQuestionId(MeetingRoom room) {
+        long count = meetingRoomQuestionRepository.count();
+        int questionId = room.getQuestion().getQuestionId();
+        if (questionId >= count) questionId = 0;
+        questionId++;
+        return questionId;
     }
 
     private void checkQuestion(Optional<MeetingRoomQuestion> question) {
